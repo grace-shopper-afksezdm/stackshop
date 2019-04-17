f8630c6f22df6803f78c173182c96774ec073e6c
Xiaoyu Zhu <emmazxy@gmail.com>
2019-04-17 15:27:29 -0400

    complete get singleProduct route and test spec

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,21 @@ const router = require('express').Router()
 const Product = require('../db/models/product')
 module.exports = router
 
+router.get('/:productId', async(req, res, next)=> {
+  try {
+
+    const singleProduct = await Product.findByPk(req.params.productId);
+    if (!singleProduct) {
+      res.sendStatus(404)
+    } else {
+      res.json(singleProduct)
+    }
+
+  } catch(err){
+    next(err)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const allProducts = await Product.findAll()
--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -30,4 +30,36 @@ describe('Product routes', () => {
       expect(res.body[0].cost).to.be.equal(testProductCost)
     })
   }) // end describe('/api/products')
+
+  describe('/api/products/:productId', () => {
+    const testProductName1 = 'daffodil'
+    const testProductCost1 = 3.99
+    const testProductId1 = 1
+    const testProductName2 = 'rose'
+    const testProductCost2 = 5.99
+    const testProductId2 = 2
+
+    beforeEach(() => {
+      return Product.bulkCreate([{
+        name: testProductName1,
+        cost: testProductCost1,
+        id: testProductId1
+      }, {
+        name: testProductName2,
+        cost: testProductCost2,
+        id: testProductId2
+      }])
+    })
+
+    it('GET /api/products/:productId', async () => {
+      const res = await request(app)
+        .get('/api/products/2')
+        .expect(200)
+
+      // expect(res.body).to.be.an('array')
+      expect(res.body.name).to.be.equal(testProductName2)
+      expect(res.body.cost).to.be.equal(testProductCost2)
+    })
+  }) //end describe("/api/products/:productId")
+
 }) //end describe('Product routes')
