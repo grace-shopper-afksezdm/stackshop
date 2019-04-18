fb5681b2425fd5da005d44fe092ebc608a13ef2b
Katie Sundstrom <ksiegel@wellesley.edu>
2019-04-18 15:44:49 -0400

    addQuantitytoCart-in processing

---
+++ client/components/addQuantitytoCart.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class AddQuantityToCart extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      quantity: 0
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      quantity: event.target.value
+    })
+  }
+
+  handleSumbit(event) {
+    event.preventDefault()
+  }
+  redner() {
+    return (
+      <div>
+        <form className="addquantityform" onSubmit={this.handleSumbit}>
+          <label htmlFor="quantity">Qty:</label>
+          <input
+            type="number"
+            name="quantity"
+            value={this.state.quantity}
+            required={true}
+            onChange={this.handleChange}
+            style={{width: 150}}
+          />
+          <button type="submit">Add to Cart</button>
+        </form>
+      </div>
+    )
+  }
+}
