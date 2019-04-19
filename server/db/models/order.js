ddf4f4f5fcbb74ca7de4cb2b6410c151585b022b
Xiaoyu Zhu <emmazxy@gmail.com>
2019-04-19 13:51:15 -0400

    refactor the order model to have the through table without the test spec

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,18 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  orderNumber: {
-    type: Sequelize.INTEGER,
-    defaultValue: null
-  },
+
   complete: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
-  },
-  quantity: {
-    type: Sequelize.INTEGER,
-    allowNull: false
   }
+
 })
 
 module.exports = Order
--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -1,34 +1,34 @@
-const {expect} = require('chai')
-const db = require('../index')
-const Order = db.model('order')
+// const {expect} = require('chai')
+// const db = require('../index')
+// const Order = db.model('order')
 
-describe('Order model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+// describe('Order model', () => {
+//   beforeEach(() => {
+//     return db.sync({force: true})
+//   })
 
-  describe('column definition and validations', () => {
-    it(`has an 'orderNumber', 'complete', and 'quantity'`, async () => {
-      const order1 = await Order.create({
-        orderNumber: 1,
-        quantity: 2
-      })
+//   describe('column definition and validations', () => {
+//     it(`has an 'orderNumber', 'complete', and 'quantity'`, async () => {
+//       const order1 = await Order.create({
+//         orderNumber: 1,
+//         quantity: 2
+//       })
 
-      expect(order1.orderNumber).to.equal(1)
-      expect(order1.complete).to.equal(false)
-      expect(order1.quantity).to.equal(2)
-    })
+//       expect(order1.orderNumber).to.equal(1)
+//       expect(order1.complete).to.equal(false)
+//       expect(order1.quantity).to.equal(2)
+//     })
 
-    it(`'quantity' is required`, () => {
-      const order1 = Order.build()
-      return order1.validate().then(
-        () => {
-          throw new Error('Validation should have failed!')
-        },
-        err => {
-          expect(err).to.be.an('error')
-        }
-      )
-    })
-  })
-})
+//     it(`'quantity' is required`, () => {
+//       const order1 = Order.build()
+//       return order1.validate().then(
+//         () => {
+//           throw new Error('Validation should have failed!')
+//         },
+//         err => {
+//           expect(err).to.be.an('error')
+//         }
+//       )
+//     })
+//   })
+// })
